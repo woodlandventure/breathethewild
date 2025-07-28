9c21f9cbe5e3b6169d55311a0430bee70c7e3326
Charlie Crisp <charlie@polymath.how>
2025-07-28 21:08:55 +0100

    Just put plain poster on

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { css } from "../styled-system/css";
-import BackgroundImage from "./assets/BackgroundImage.jpg";
-import LogoImage from "./assets/LogoImage.jpg";
+import FullPoster from "./assets/FullPoster.jpg";
 import "./index.css";
 
 function App() {
@@ -9,9 +8,10 @@ function App() {
       className={css({
         height: "100vh",
         width: "100vw",
-        bg: "#f5f5dc",
+        bg: "#787864",
         position: "absolute",
         display: "flex",
+        justifyContent: "center",
         top: 0,
         left: 0,
         right: 0,
@@ -21,13 +21,8 @@ function App() {
       {/* Background Image */}
       <div
         className={css({
-          position: "absolute",
+          position: "relative",
           height: "100%",
-          width: "100%",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
@@ -35,7 +30,7 @@ function App() {
         })}
       >
         <img
-          src={BackgroundImage}
+          src={FullPoster}
           alt="Background"
           className={css({
             height: "100%",
@@ -44,70 +39,6 @@ function App() {
             objectPosition: "center",
           })}
         />
-      </div>
-
-      {/* Cream Overlay */}
-      <div
-        className={css({
-          position: "absolute",
-          height: "100%",
-          width: "100%",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          bg: "#f5f5dc",
-          opacity: "0.3",
-          zIndex: "1",
-        })}
-      />
-
-      {/* Content */}
-      <div
-        className={css({
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: "2",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          textAlign: "center",
-          px: "4",
-        })}
-      >
-        <div
-          className={css({
-            px: "4",
-          })}
-        >
-          <h1
-            className={css({
-              fontSize: { base: "3xl", md: "5xl", lg: "6xl" },
-              fontWeight: "bold",
-              color: "#1a1a1a",
-              margin: "0",
-              mb: "4",
-              textShadow: "2px 2px 4px rgba(0,0,0,0.1)",
-            })}
-          >
-            Breathe The Wilde
-          </h1>
-          <p
-            className={css({
-              fontSize: { base: "xl", md: "2xl", lg: "3xl" },
-              color: "#374151",
-              margin: "0",
-              fontWeight: "medium",
-              textShadow: "1px 1px 2px rgba(0,0,0,0.1)",
-            })}
-          >
-            Coming Soon
-          </p>
-        </div>
       </div>
     </div>
   );
